682d67bac6e8cb68c9f5592ed3ce2cb7428cf745
Alex Liang <alex.c.liang@gmail.com>
2016-12-04 14:21:15 -0500

    added normalize css

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
+/* eslint-disable import/no-unresolved */
+require('!!style-loader!css-loader!normalize.css/normalize.css')
+/* eslint-enable import/no-unresolved */
+
 const rootEl = document.getElementById('root')
 
 const renderApp= (Content,element) => {
